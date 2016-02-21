2b1958927f1766cc3f9473b37b59a3234cd0f574
vera boteva <vera boteva>
2016-02-21 14:10:58 +0100

    refactored WirdNetSearcher: runtime

--- WordNetSearcher.py
+++ WordNetSearcher.py
@@ -5,7 +5,9 @@ from Word import Word
 import sys
 
 class WordNetSearcher:
-	indexFiles = ["data/index.noun", "data/index.verb", "data/index.adj", "data/index.adv"]
+	indexFiles = ["data/index.noun", "data/index.verb", "data/index.adv", "data/index.adj"]
+	# key: word, value: list of synsets
+	indexDictionary = {}
 	#input: list of word objects
 	def __init__(self, inputWords):
 		self.inputWords = inputWords
@@ -19,39 +21,44 @@ class WordNetSearcher:
 		return self.inputWords
 	def setWords(self, inputWords):
 		self.inputWords = inputWords
-		
-	def getMeanings(self):
-		for word in self.inputWords:
-			if word.getName():
-				self.readIndexFile(word)
-			else:
-				sys.stderr.write("this word is empty")
 	
-	def readIndexFile(self, word):
-		wordfoundInWordnet = False
+	@staticmethod
+	def getIndexDictionary():
+		return WordNetSearcher.indexDictionary
+	
+	@staticmethod
+	def readIndexFile():
 		for indexFile in WordNetSearcher.indexFiles:
 			file = open(indexFile, 'r')
 			lines = file.readlines()
 			for line in lines:
-				elements = line.strip(' \t\n\r').split(" ")
-				if(elements[0] == word.getName().lower()):
-					self.findSynsetsForWord(word, elements)
-					wordfoundInWordnet = True
-		if wordfoundInWordnet == False:
-			sys.stderr.write("The word: " +  word.getName() + " was not found in WordNet 2.1")
-			word.setDeadWord(True)
-	
-	def findSynsetsForWord(self, word, lineAsList):
+				if not line.startswith("  "):
+					elements = line.strip(" \t\n\r").split(" ")
+					WordNetSearcher.findAllSynsetsForIndexLine(elements)
+	@staticmethod	
+	def findAllSynsetsForIndexLine(lineAsList):
 		lemma = lineAsList[0]
 		pnt_count = lineAsList[3]
 		indexOfFirstSynset = 3 + int(pnt_count) + 3 # pnt_count is at index 3 + relations + 2 for synset count and target_synset count + 1 -> first index of synset
-		synsets = lineAsList[indexOfFirstSynset:len(lineAsList)]
-		if not synsets:
-			word.setDeadWord(True)
+		synsets = set(lineAsList[indexOfFirstSynset:len(lineAsList)])
+		dict = WordNetSearcher.getIndexDictionary() 
+		if lemma in dict:
+			#print lemma + " " + str(dict[lemma])
+			dict[lemma].update(synsets)
 		else:
-			word.getSynsets().extend(synsets)
-			self.getSynsets().update(synsets)
-			
+			WordNetSearcher.getIndexDictionary().setdefault(lemma, synsets)
+	
+	def getMeaningsForAllWords(self):
+		for word in self.inputWords:
+			wordfoundInWordnet = False
+			lemma = word.getName()
+			dict = WordNetSearcher.getIndexDictionary()
+			if lemma and lemma in dict:
+				word.getSynsets().update(dict[lemma])
+				self.getSynsets().update(dict[lemma])
+			else:
+				word.setDeadWord(True)
+				sys.stderr.write("This word is empty or was not found in WordNet 2.1")
 
 if __name__ == "__main__":
 	#mock of word objects as input
@@ -60,7 +67,14 @@ if __name__ == "__main__":
 	word3 = Word("arctonyx collaris")
 	listOfAnimals = [word1, word2, word3] # reference to object
 	wordsearcher = WordNetSearcher(listOfAnimals)
-	wordsearcher.getMeanings()
-	for word in listOfAnimals:
-		print(word.getName())
-		print(word.getSynsets())
+	
+	WordNetSearcher.readIndexFile()
+	wordsearcher.getMeaningsForAllWords()
+	print wordsearcher.getSynsets()
+	# d = set()
+	# d.add(1)
+	# print(d)
+	# list = [1,2,3,4]
+	# s = set(list)
+	# s.update([5, 4])
+	# print(s)
