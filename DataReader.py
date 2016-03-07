ff0f2f542181a3f628b03dc7d44eb0441f2572fe
Earthhorn <kilghard2@gmx.de>
2016-03-07 02:14:05 +0100

    Reader 2/3

--- DataReader.py
+++ DataReader.py
@@ -1,4 +1,5 @@
 import Lemma
+import Translation
 
 class Reader():
 	def __init__(self, file, type):
@@ -35,23 +36,45 @@ class Reader():
 		# Closes automatically
 		with open(self.filename) as f:
 			self.rawcontent = [line.strip(' \t\n\r') for line in f]
+
 			
 	# Process the raw data into only useful information
 	def processContent(self):
 		if self.filetype == "ON":
 			# Kick the useless first entry
-			for b in range(1,len(self.rawcontent)):
+			for a in range(1,len(self.rawcontent)):
 				# Kick useless empty lines
-				if self.rawcontent[b] != "":
-					splitLine = self.rawcontent[b].split()
+				if self.rawcontent[a] != "":
+					splitLine = self.rawcontent[a].split()
 					# Pick out the juicy bits
 					dummy_lemma = Lemma.Lemma(int(splitLine[1]), int(splitLine[2]), str(splitLine[3]), str(splitLine[4]), str(splitLine[6]), str(splitLine[8]))
 					self.procontent.append(dummy_lemma)
+		if self.filetype == "IN":
+			# Kick the useless first 29 entries
+			for a in range(29,len(self.rawcontent)):
+				# Kick useless empty lines
+				if self.rawcontent[a] != "":
+					splitLine = self.rawcontent[a].split()
+					# Pick out the juicy bits
+					dummy_list_of_senses = []
+					# Check for multiple senses
+					for b in range(6,len(splitLine)):
+						if len(splitLine[b]) > 3:
+							dummy_list_of_senses.append(splitLine[b])
+					# Putting juicy bits together
+					dummy_translation = Translation.Translation(str(splitLine[0]), str(splitLine[1]), dummy_list_of_senses)
+					self.procontent.append(dummy_translation)
+					
+				
 	
 	# >>>FOR TESTING ONLY<<<
 	def translateProcontent(self):
-		for a in range(len(self.procontent)):
-			print(self.procontent[a].returnLemma())
+		if self.filetype == "ON":
+			for a in range(len(self.procontent)):
+				print(self.procontent[a].returnLemma())
+		if self.filetype == "IN":
+			for a in range(len(self.procontent)):
+				print(self.procontent[a].returnTranslation())	
 
 if __name__ == "__main__":
 	
@@ -59,32 +82,60 @@ if __name__ == "__main__":
 	# WARNING	absolute path!	WARNING
 	# WARNING		WARNING		WARNING
 	
-	reader1 = Reader("D:/TEST/wsd_pageRank/reader_test.txt","ON")
+	reader1 = Reader("D:/TEST/test.txt","ON")
+	reader2 = Reader("D:/TEST/index.adj","IN")
 	
-	print
-	print "SETUP STARTS"
-	print
-	print "Location:"
-	print(reader1.returnFilename())
-	print "Type:"
-	print(reader1.returnFiletype())
-	print "(Emtpy) Rrw:"
-	print(reader1.returnRawcontent())
-	print "(Empty) pro:"
-	print(reader1.returnProcontent())
-	print
-	print "READING STARTS"
-	print
-	reader1.readFile()
-	print "New raw:"
-	print(reader1.returnRawcontent())
-	print
-	print "PROCESSING STARTS"
-	print
-	reader1.processContent()
-	print "New pro:"
-	print(reader1.returnProcontent())
-	print
-	print "<Translating>"
-	reader1.translateProcontent()
+#	print
+#	print "SETUP STARTS"
+#	print
+#	print "Location:"
+#	print(reader1.returnFilename())
+#	print "Type:"
+#	print(reader1.returnFiletype())
+#	print "(Emtpy) Rrw:"
+#	print(reader1.returnRawcontent())
+#	print "(Empty) pro:"
+#	print(reader1.returnProcontent())
+#	print
+#	print "READING STARTS"
+#	print
+#	reader1.readFile()
+#	print "New raw:"
+#	print(reader1.returnRawcontent())
+#	print
+#	print "PROCESSING STARTS"
+#	print
+#	reader1.processContent()
+#	print "New pro:"
+#	print(reader1.returnProcontent())
+#	print
+#	print "<Translating>"
+#	reader1.translateProcontent()
+
+#	print
+#	print "SETUP STARTS"
+#	print
+#	print "Location:"
+#	print(reader2.returnFilename())
+#	print "Type:"
+#	print(reader2.returnFiletype())
+#	print "(Emtpy) Rrw:"
+#	print(reader2.returnRawcontent())
+#	print "(Empty) pro:"
+#	print(reader2.returnProcontent())
+#	print
+#	print "READING STARTS"
+#	print
+#	reader2.readFile()
+#	print "New raw:"
+#	print(reader2.returnRawcontent())
+#	print
+#	print "PROCESSING STARTS"
+#	print
+#	reader2.processContent()
+#	print "New pro:"
+#	print(reader2.returnProcontent())
+#	print
+#	print "<Translating>"
+#	reader2.translateProcontent()
 	
